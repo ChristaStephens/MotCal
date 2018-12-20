8a30736ba27f23c9a764c5e0c74eb59f5e02b6ef
Christa Stephens <ChristaStephens@outlook.com>
2018-12-20 17:07:10 -0500

    initial commit of gradle build for Motivational Calendar

---
+++ src/main/java/MainApp/CalApp.java
@@ -0,0 +1,35 @@
+package MainApp;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalApp {
+
+	public static void main(String[] args) {
+				// Goal is to create a motivational quote app
+				// with each day/quote refreshing and potentially
+				// giving a new background & quote.
+
+				// String day = new SimpleDateFormat("EEEE").format(new Date());
+				System.out.println(thisIsToday());
+
+				// String timeStamp = new SimpleDateFormat("MM/dd/YYYY hh:mm
+				// a").format(Calendar.getInstance().getTime());
+				System.out.println(dayWithTime());
+
+			}
+
+			protected static String thisIsToday() {
+				String day = new SimpleDateFormat("EEEE").format(new Date());
+
+				return day;
+
+			}
+
+			protected static String dayWithTime() {
+				String timeStamp = new SimpleDateFormat("MM/dd/YYYY hh:mm a").format(Calendar.getInstance().getTime());
+				return timeStamp;
+			}
+
+}
